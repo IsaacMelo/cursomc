56817d2577516e0ed92415c2aad0ca3cb4f7f0d0
isaac.melo <isaacjardim@hotmail.com>
2017-11-30 19:13:55 -0200

    Criando operacao de inicializacao

--- src/main/java/com/isaacmelo/CursomcApplication.java
+++ src/main/java/com/isaacmelo/CursomcApplication.java
@@ -1,12 +1,31 @@
 package com.isaacmelo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.isaacmelo.domain.Categoria;
+import com.isaacmelo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	public CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }
--- src/main/java/com/isaacmelo/resources/CategoriaResource.java
+++ src/main/java/com/isaacmelo/resources/CategoriaResource.java
@@ -1,7 +1,5 @@
 package com.isaacmelo.resources;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.isaacmelo.domain.Categoria;
 import com.isaacmelo.services.CategoriaService;
 
 /**
