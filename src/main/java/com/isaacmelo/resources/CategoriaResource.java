c7f84ac1b2c8490fd4903e496721297d3f9c4533
isaac.melo <isaacjardim@hotmail.com>
2017-11-30 13:07:11 -0200

    Testando o REST

---
+++ src/main/java/com/isaacmelo/resources/CategoriaResource.java
@@ -0,0 +1,19 @@
+package com.isaacmelo.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author isaac.melo
+ *
+ */
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest esta funcionando";
+	}
+}
